ad76c5f0f0ccea81c5d74e31edc68ad0876f3a8f
yuto.moriizumi <kurvan1112@gmail.com>
2023-04-18 18:05:09 +0900

    基本的なTODO機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
 import { useState } from "react";
 
+type Todo = {
+  title: string;
+  content: string;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const addTodo = () => {
+    setTodos([...todos, { title, content }]);
+    setTitle("");
+    setContent("");
+  };
+  const deleteTodo = (index: number) => {
+    setTodos(todos.filter((t, i) => i != index));
+  };
 
   return (
     <>
-      <h1>Hello world from React</h1>
-      <p>with Electron Forge!</p>
-      <p>You clicked {count} time(s)</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>Todo application</h1>
+      {todos.map((t, i) => (
+        <tr>
+          <td>{t.title}</td>
+          <td>{t.content}</td>
+          <td>
+            <button onClick={() => deleteTodo(i)}>削除</button>
+          </td>
+        </tr>
+      ))}
+      <div>
+        <div>
+          <label htmlFor="">タイトル</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="">内容</label>
+          <input
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button onClick={addTodo}>追加</button>
+      </div>
     </>
   );
 }
